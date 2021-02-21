3259c27bc55871b261bdff1158262bfca45b7bb3
Andy Pak <skandla2002@gmail.com>
2021-02-21 17:03:15 +0900

    Content 내용 개선

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,45 @@
+import { Fragment } from 'react';
+import { eventNames as events } from './consts';
+
+const {
+    MOUSE_DOWN
+} = events;
+
+
 class Content {
     
     constructor(){
-
+        this.state = {
+            text: 'default text',
+            button: ['first', 'second', 'third']
+        }
     }
 
+    onClickRandom = (evt) => {
+        console.log(evt);
+    }
+
+    render(){
+        return (
+            <Fragment>
+                {this.state.button && 
+                <nav>
+                    <ol>
+                        {
+                            this.state.button.map((data) => {
+                                return <li>{data}</li>
+                            })
+                        }
+                    </ol>
+                </nav>
+                }
+                <article>
+                    {this.state.text}
+                </article>
+                <button onClick={this.onClickRandom} />
+            </Fragment>
+        )
+    }
     
 }
 
